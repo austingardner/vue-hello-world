1a104f4bbd28b56a98e3f37362aee32df4fd4da6
Austin Gardner <gardneraustin@protonmail.com>
2022-02-12 20:33:49 -0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" width="25%" />
+    <div>topCount: {{ topCount }}</div>
+    <HelloWorld msg="Hello Vue in CodeSandbox!" />
+    <MyInput
+      v-on:topcountup="increment"
+      :topCount="topCount"
+      placeholder="I am a placeholder"
+    />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+import MyInput from "./components/MyInput";
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    MyInput,
+  },
+  data() {
+    return {
+      topCount: 0,
+    };
+  },
+  methods: {
+    increment() {
+      this.topCount++;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/MyInput.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div>Cool Beans, let's start the input</div>
+    <input @input="handleChange" :placeholder="placeholder" />
+    <div>Value: {{ value }}</div>
+    <button @click="increment">Increment Top Count</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    placeholder: String,
+    topCount: Number,
+  },
+  data() {
+    return {
+      value: "",
+      count: this.topCount,
+    };
+  },
+  methods: {
+    handleChange(e) {
+      this.value = e.target.value;
+    },
+    increment() {
+      this.$emit("topcountup");
+    },
+  },
+};
+</script>
